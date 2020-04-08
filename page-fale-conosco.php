bbab737ba250d27e9ef03d8efdff69f0731cc5c2
Eder Soares <edersoares@me.com>
2020-04-08 17:17:14 -0300

    Ajusta fale conosco

--- page-fale-conosco.php
+++ page-fale-conosco.php
@@ -5,13 +5,16 @@ get_header(); ?>
     <div class="container">
       <div class="row wow animated fadeIn">
         <div class="col-sm-6">
-          <h1 class="mb-4">Fale com a a gente!</h2>
+          <h1 class="mb-4">Fale com a gente!</h1>
           <p class="subtitle mb-4">Informações de contato</p>
           <p><strong>Telefone</strong><br>
             (48) 3082-9085
           </p>
           <p><strong>E-mail</strong><br>
             contato@portabilis.com.br
+          </p>
+          <p><strong>Endereço</strong><br>
+            Rua Vitória, 538 Edifício Dionísio Pavei 2º piso - Centro, Içara - SC, 88820-000
           </p>
           <p class="mb-1"><strong>Redes Sociais</strong></p>
           <ul class="redes">
