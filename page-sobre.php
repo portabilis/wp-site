1c6b75c5c9e74221422ecc76a6b23243857aa900
Eder Soares <edersoares@me.com>
2020-04-16 15:17:05 -0300

    Logo do i-Educar

--- page-sobre.php
+++ page-sobre.php
@@ -201,7 +201,7 @@ uso do i-Educar”</h2>
         </div>
       </div>
       <div class="row wow animated fadeInUp">
-        <div class="col-lg-4 offset-4">
+        <div class="col-lg-4 offset-2">
           <?php if(!empty(get_field('material_para_imprensa_1'))): ?>
           <a target="_blank" class="archive d-flex justify-content-between align-items-center" href="<?php the_field('material_para_imprensa_1'); ?>">
             <div class="info d-flex">
@@ -224,6 +224,28 @@ uso do i-Educar”</h2>
             </div>
           </a>
           <?php endif; ?>
+        </div>
+        <div class="col-lg-4">
+            <a target="_blank" class="archive d-flex justify-content-between align-items-center" href="/wp-content/uploads/2020/04/Logo.png">
+              <div class="info d-flex">
+                <img class="pr-3" src="<?php echo get_site_url();?>/wp-content/uploads/2019/07/img-export-pdf-portabilis.png">
+                <div class="d-flex flex-column text-left">
+                  <p class="mb-0"> Logo i-Educar</p>
+                  <span>.png .svg. .jpg .eps</span>
+                </div>
+              </div>
+              <div class="icon">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="4" y="4" width="16" height="16">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M12 4C11.4477 4 11 4.44772 11 5V16.5858L5.70711 11.2929C5.31658 10.9024 4.68342 10.9024 4.29289 11.2929C3.90237 11.6834 3.90237 12.3166 4.29289 12.7071L11.2923 19.7065L11.2929 19.7071C11.2951 19.7093 11.2973 19.7115 11.2995 19.7136C11.297 19.7112 11.2947 19.7089 11.2923 19.7065M12.7076 19.7066L19.7071 12.7071C20.0976 12.3166 20.0976 11.6834 19.7071 11.2929C19.3166 10.9024 18.6834 10.9024 18.2929 11.2929L13 16.5858V5C13 4.44772 12.5523 4 12 4" fill="white"/>
+                  </mask>
+                  <g mask="url(#mask0)">
+                    <rect width="32" height="32" transform="translate(-4 -4)" fill="white"/>
+                  </g>
+                </svg>
+
+              </div>
+            </a>
         </div>
       </div>
     </div>
